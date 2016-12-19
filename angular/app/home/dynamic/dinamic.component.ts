066c11d2323def17eefb50f52307b2f8e9b8f4cd
Toni <toni@thebrightbox.com>
2016-12-19 12:27:02 +0100

    make example test for async/await method

--- angular/app/home/dynamic/dinamic.component.ts
+++ angular/app/home/dynamic/dinamic.component.ts
@@ -9,7 +9,9 @@ export function dinamicComponent(tmpl) {
         template: tmpl,
     })
     class CustomDynamicComponent  implements OnInit {
-        constructor(private el: ElementRef) {}
+        constructor(private el: ElementRef) {
+            this.asyncAwait();
+        }
 
         ngOnInit() {
             const hostElem = this.el.nativeElement;
@@ -18,6 +20,29 @@ export function dinamicComponent(tmpl) {
 
         example() {
             console.log('example click');
+        }
+
+        /**
+         * @returns {Promise<void>}
+         */
+        async asyncAwait() {
+            console.log("Knock, knock!");
+
+            await this.delay(1000);
+            console.log("Who's there?");
+
+            await this.delay(1000);
+            console.log("async/await!");
+        }
+
+        /**
+         * @param ms
+         * @returns {Promise<void>}
+         */
+        private delay(ms: number) {
+            return new Promise<void>(function(resolve) {
+                setTimeout(resolve, ms);
+            });
         }
     }
 
